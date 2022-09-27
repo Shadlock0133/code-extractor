61109c0a146a8a850343149f6397db1bfc178c84
Aurora <tomek-kubel2@wp.pl>
2022-09-27 22:08:35 +0200

    add more items

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,51 @@ use std::{fs, path::PathBuf};
 use clap::{Parser, Subcommand};
 use owo_colors::OwoColorize;
 use prettyplease::unparse;
-use syn::{File, Item, ItemEnum, ItemFn, ItemStruct};
+use syn::{
+    File, Item, ItemConst, ItemEnum, ItemExternCrate, ItemFn, ItemMacro,
+    ItemStatic, ItemStruct, ItemTrait, ItemType, ItemUnion,
+};
 
 #[derive(Subcommand)]
 enum ExtractItem {
     #[clap(name = "list")]
     ListItems,
     #[clap(alias = "f")]
-    Function { name: String },
+    Function {
+        name: String,
+    },
     #[clap(alias = "s")]
-    Struct { name: String },
+    Struct {
+        name: String,
+    },
     #[clap(alias = "e")]
-    Enum { name: String },
+    Enum {
+        name: String,
+    },
+    #[clap(alias = "t")]
+    Trait {
+        name: String,
+    },
+    #[clap(alias = "c")]
+    Const {
+        name: String,
+    },
+    ExternCrate {
+        name: String,
+    },
+    Static {
+        name: String,
+    },
+    Type {
+        name: String,
+    },
+    Union {
+        name: String,
+    },
+    /// Note: output might be mangled
+    Macro {
+        name: String,
+    },
 }
 
 #[derive(Parser)]
@@ -33,10 +66,18 @@ fn main() {
             println!("Listing items:");
             for item in file.items {
                 let info = match item {
-                    Item::Fn(f) => Some(("fn", f.sig.ident)),
-                    Item::Struct(s) => Some(("struct", s.ident)),
-                    Item::Enum(e) => Some(("enum", e.ident)),
-                    Item::Trait(t) => Some(("trait", t.ident)),
+                    Item::Fn(i) => Some(("fn", i.sig.ident)),
+                    Item::Struct(i) => Some(("struct", i.ident)),
+                    Item::Enum(i) => Some(("enum", i.ident)),
+                    Item::Trait(i) => Some(("trait", i.ident)),
+                    Item::Const(i) => Some(("const", i.ident)),
+                    Item::ExternCrate(i) => Some(("extern crate", i.ident)),
+                    Item::Static(i) => Some(("static", i.ident)),
+                    Item::Type(i) => Some(("type", i.ident)),
+                    Item::Union(i) => Some(("union", i.ident)),
+                    Item::Macro(ItemMacro { ident: Some(i), .. }) => {
+                        Some(("macro", i))
+                    }
                     _ => None,
                 };
                 if let Some((kind, name)) = info {
@@ -52,6 +93,27 @@ fn main() {
         }
         ExtractItem::Enum { name } => {
             print!("{}", extract::<ItemEnum>(&file, &name))
+        }
+        ExtractItem::Trait { name } => {
+            print!("{}", extract::<ItemTrait>(&file, &name))
+        }
+        ExtractItem::Const { name } => {
+            print!("{}", extract::<ItemConst>(&file, &name))
+        }
+        ExtractItem::ExternCrate { name } => {
+            print!("{}", extract::<ItemExternCrate>(&file, &name))
+        }
+        ExtractItem::Static { name } => {
+            print!("{}", extract::<ItemStatic>(&file, &name))
+        }
+        ExtractItem::Type { name } => {
+            print!("{}", extract::<ItemType>(&file, &name))
+        }
+        ExtractItem::Union { name } => {
+            print!("{}", extract::<ItemUnion>(&file, &name))
+        }
+        ExtractItem::Macro { name } => {
+            print!("{}", extract::<ItemMacro>(&file, &name))
         }
     }
 }
@@ -72,39 +134,6 @@ trait Find {
     fn find_item<'a>(item: &'a Item, name: &str) -> Option<&'a Self>;
 }
 
-impl Find for ItemFn {
-    fn find_item<'a>(item: &'a Item, name: &str) -> Option<&'a Self> {
-        if let Item::Fn(f) = item {
-            if f.sig.ident == name {
-                return Some(f);
-            }
-        }
-        None
-    }
-}
-
-impl Find for ItemStruct {
-    fn find_item<'a>(item: &'a Item, name: &str) -> Option<&'a Self> {
-        if let Item::Struct(s) = item {
-            if s.ident == name {
-                return Some(s);
-            }
-        }
-        None
-    }
-}
-
-impl Find for ItemEnum {
-    fn find_item<'a>(item: &'a Item, name: &str) -> Option<&'a Self> {
-        if let Item::Enum(s) = item {
-            if s.ident == name {
-                return Some(s);
-            }
-        }
-        None
-    }
-}
-
 trait Unparse: Sized {
     fn as_item(self) -> Item;
     fn unparse(self) -> String {
@@ -118,20 +147,66 @@ trait Unparse: Sized {
     }
 }
 
+macro_rules! impl_traits {
+    ($t:ty : Item:: $var:ident) => {
+        impl Find for $t {
+            fn find_item<'a>(item: &'a Item, name: &str) -> Option<&'a Self> {
+                if let Item::$var(i) = item {
+                    if i.ident == name {
+                        return Some(i);
+                    }
+                }
+                None
+            }
+        }
+
+        impl Unparse for $t {
+            fn as_item(self) -> Item {
+                Item::$var(self)
+            }
+        }
+    };
+}
+
+impl Find for ItemFn {
+    fn find_item<'a>(item: &'a Item, name: &str) -> Option<&'a Self> {
+        if let Item::Fn(f) = item {
+            if f.sig.ident == name {
+                return Some(f);
+            }
+        }
+        None
+    }
+}
+
 impl Unparse for ItemFn {
     fn as_item(self) -> Item {
         Item::Fn(self)
     }
 }
 
-impl Unparse for ItemStruct {
-    fn as_item(self) -> Item {
-        Item::Struct(self)
+impl Find for ItemMacro {
+    fn find_item<'a>(item: &'a Item, name: &str) -> Option<&'a Self> {
+        if let Item::Macro(f) = item {
+            if f.ident.as_ref()? == name {
+                return Some(f);
+            }
+        }
+        None
     }
 }
 
-impl Unparse for ItemEnum {
+impl Unparse for ItemMacro {
     fn as_item(self) -> Item {
-        Item::Enum(self)
+        Item::Macro(self)
     }
 }
+
+impl_traits!(ItemStruct: Item::Struct);
+impl_traits!(ItemEnum: Item::Enum);
+impl_traits!(ItemTrait: Item::Trait);
+impl_traits!(ItemConst: Item::Const);
+impl_traits!(ItemExternCrate: Item::ExternCrate);
+impl_traits!(ItemStatic: Item::Static);
+impl_traits!(ItemType: Item::Type);
+impl_traits!(ItemUnion: Item::Union);
